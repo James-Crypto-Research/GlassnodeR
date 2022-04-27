0c820bd0be194bb738fb8c1a6e057f8efd1fe8df
James Chapman <james@paymenteconomics.com>
2022-04-27 06:25:01 +0200

    added a marketcap function

---
+++ R/get_marketcap.R
@@ -0,0 +1,37 @@
+#' Get the Marketcap of a toke
+#'
+#' @param api_key The API key to use. By default it will check the API_KEY environmental variable
+#' @param asset This is the asset to get information on. The list of available assets is on the GN API site
+#' @param since,until A POSIX compatible date-time object. It's converted to a unix date number
+#' @param frequency A resolution for the data. See API documentation but it defaults to 24h
+#' @param as_date A logical to return a date-time object or a date object for daily observations
+#'
+#' @return Returns a tibble with columns for the datetime and marketcap
+#' @export
+#' @importFrom rlang :=
+#' @examples
+#' \dontrun{
+#' #Need a valid API key to run
+#' x <- get_marketcap()
+#' }
+get_marketcap <- function(asset="BTC",since=NULL,until=NULL,
+                          frequency="24h",
+                          api_key = Sys.getenv("GN_API_KEY"),
+                          as_date=TRUE){
+  tmp <- list("a" = asset,
+              "s" = since,
+              "u" = until,
+              "i" = frequency,
+              "api_key" = api_key)
+  params <- do.call(make_params, tmp)
+  x <- call_glassnode_api(
+    path = glue::glue("v1/metrics/market/marketcap_usd"), params
+  ) |>
+    tibble::as_tibble() |>
+    dplyr::rename(date=t, {{asset}} :=v) |>
+    dplyr::mutate(date=as.POSIXct(date,origin="1970-01-01 00:00:00"))
+  if (as_date & frequency == "24h"){
+    x$date <- as.Date(x$date)
+  }
+  return(x)
+}
