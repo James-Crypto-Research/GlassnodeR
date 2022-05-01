24b7a9e1684794d988bbf723a4a52daaa81e0ecf
James Chapman <james@paymenteconomics.com>
2022-05-01 09:47:21 -0400

    Fixed the documentation
    Fixed the documentation of the mining functions

--- R/mining.R
+++ R/mining.R
@@ -2,21 +2,32 @@
 # This contains the functions to grab miner data
 
 
-#' Title
+#' Get revenue (fee and block reward) for miners
 #'
-#' @param chain
-#' @param since
-#' @param until
-#' @param frequency
-#' @param get_agg
-#' @param currency
-#' @param api_key
-#' @param as_date
+#' These functions grab the revenue in USD of blocks of miners. The revenue
+#' comes from fees and from block rewards. The function get_revenue_miners
+#' returns either jsut the aggregated (if get_agg is true) or all miner
+#' revenues. Individual miners revenue can be found using get_miner_rev and
+#' passing the name of the miner.
 #'
-#' @return
+#' NOTE: Aggregated miner revenue is available for ETH and BTC but individual
+#' miner revenue is only available for BTC.
+#'
+#'
+#'
+#' @param chain The chain to use ("BTC" or "ETH")
+#' @param since The date to start the extraction
+#' @param until The date to end the extraction
+#' @param frequency What frequency to pull (either "1h" or "24h")
+#' @param get_agg A logical for whether to get all miners
+#' @param currency Should the value be returned in "USD" or "NATIVE"
+#' @param api_key The API key. It defaults to the GN_API_KEY environmental variables
+#' @param as_date A logical whether to return a date object if the frequency is "24h"
+#' @param miner The name of the miner to use
+#'
+#' @return This returns a
 #' @export
 #'
-#' @examples
 get_revenue_miners <- function(chain="BTC",
                                since=NULL,until=NULL,
                                         frequency="24h",
@@ -45,21 +56,7 @@ get_revenue_miners <- function(chain="BTC",
 }
 
 
-#' Title
-#'
-#' @param miner
-#' @param chain
-#' @param since
-#' @param until
-#' @param frequency
-#' @param currency
-#' @param api_key
-#' @param as_date
-#'
-#' @return
-#' @export
-#'
-#' @examples
+#' @rdname get_revenue_miners
 get_miner_rev <- function(miner="aggregated",
                           chain="BTC",
                           since=NULL,
@@ -91,19 +88,25 @@ get_miner_rev <- function(miner="aggregated",
 
 
 
-#' Title
+#' Get volume mined  for BTC miners
 #'
-#' @param since
-#' @param until
-#' @param frequency
-#' @param get_agg
-#' @param api_key
-#' @param as_date
+#' These functions grab the number of coins  miners have won.
+#'  The function get_vol_miners
+#' returns either jsut the aggregated (if get_agg is true) or all miner
+#' volumes. Individual miner's volumescan be found using get_miner_vol and
+#' passing the name of the miner.
 #'
-#' @return
-#' @export
+#' NOTE: These amounts are available only for BTC.
 #'
-#' @examples
+#'
+#' @param since The date to start the extraction
+#' @param until The date to end the extraction
+#' @param frequency What frequency to pull (either "1h" or "24h")
+#' @param get_agg A logical for whether to get all miners
+#' @param api_key The API key. It defaults to the GN_API_KEY environmental variables
+#' @param as_date A logical whether to return a date object if the frequency is "24h"
+#' @param miner The name of the miner to use
+#'
 get_vol_miners <- function(since=NULL,until=NULL,
                                frequency="24h",
                                get_agg=TRUE,
@@ -129,19 +132,7 @@ get_vol_miners <- function(since=NULL,until=NULL,
   return(x)
 }
 
-#' Title
-#'
-#' @param miner
-#' @param since
-#' @param until
-#' @param frequency
-#' @param api_key
-#' @param as_date
-#'
-#' @return
-#' @export
-#'
-#' @examples
+#' @rdname get_vol_miners
 get_miner_vol <- function(miner="aggregated",
                           since=NULL,
                           until=NULL,
